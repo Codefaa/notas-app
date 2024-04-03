600e7ff28788bc94c552b95ecdffd118c0cb0c23
Codefaa <facundogarcia1999@outlook.com>
2024-04-03 03:58:33 -0300

    cambios..

--- frontend/src/api/auth.js
+++ frontend/src/api/auth.js
@@ -1,11 +1,11 @@
 import axios from "axios";
 
-const URL = process.env.REACT_APP_API_URL || "http://localhost:4000/api";
+const URL = process.env.REACT_APP_API_URL || "http://localhost:4000";
 
 const API = `${URL}`;
 
-export const registerRequest = async (user) => axios.post(`${API}/auth/register`, user);
+export const registerRequest = async (user) => axios.post(`${API}/api/auth/register`, user);
 
-export const loginRequest = async (user) => axios.post(`${API}/auth/login`, user);
+export const loginRequest = async (user) => axios.post(`${API}/api/auth/login`, user);
 
-export const verifyTokenRequest = async () => axios.get(`${API}/auth/verify`);
+export const verifyTokenRequest = async () => axios.get(`${API}/api/auth/verify`);
