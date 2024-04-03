c11a11e536ea87507b8880238e48b8474a873aa3
Codefaa <facundogarcia1999@outlook.com>
2024-04-03 03:42:57 -0300

    cambios front

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
-import { REACT_APP_API_URL } from "../config";
+import REACT_APP_API_URL from "../config";
 
-const URL = process.env.REACT_APP_API_URL || "http://localhost:4000"
+const URL = process.env.REACT_APP_API_URL || "http://localhost:4000/api"
 
 const instance = axios.create({
   baseURL: URL,
