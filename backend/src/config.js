cb68b2d6a8e2e3762fafb09aa13dd94976d4545b
Codefaa <facundogarcia1999@outlook.com>
2024-04-04 17:58:33 -0300

    dadas

--- backend/src/config.js
+++ backend/src/config.js
@@ -4,4 +4,4 @@ export const TOKEN_SECRET = process.env.TOKEN_SECRET || "secret";
 
 export const FRONTEND_URL = process.env.FRONTEND_URL || "http://localhost:3000";
 
-export const MONGODB_URL = process.env.MONGODB_URL || `mongodb+srv://facundogarcia999:login123@cluster0.xxtmsxd.mongodb.net/username?retryWrites=true&w=majority&appName=Cluster0`
+export const MONGODB_URL = process.env.MONGODB_URL || "mongodb+srv://facundogarcia999:login123@cluster0.xxtmsxd.mongodb.net/username?retryWrites=true&w=majority&appName=Cluster0"
