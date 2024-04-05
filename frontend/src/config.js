fc4300e382b32c29ae012583e6fe0b946bf54006
Codefaa <facundogarcia1999@outlook.com>
2024-04-05 04:13:51 -0300

    dasdada

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,1 +1,1 @@
-export const REACT_APP_API_URL = process.env.REACT_APP_API_URL || "http://localhost:4000/api";
+export const REACT_APP_API_URL = process.env.REACT_APP_API_URL || "http://localhost:4000";
